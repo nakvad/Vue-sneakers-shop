fcbc5815bf6854a5386dc4f47b78791ccd0770a1
nakvad <evgeny.busin@gmail.com>
2024-03-02 14:04:12 +0300

    create project

---
+++ vue-sneakers/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <div class="w-3/5 m-auto">
+    <h1>Archakov blog</h1>
+  </div>
+</template>
---
+++ vue-sneakers/src/components/MyHello.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Это мой компонент</p>
+    <button>Открыть</button>
+  </div>
+</template>
